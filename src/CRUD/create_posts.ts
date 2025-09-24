3c8fdcf96971df9539ab742df5cf378f5af9f78e
CiaranLy <lynchbros3@gmail.com>
2025-09-24 21:29:02 +0100

    added address to create post

--- src/CRUD/create_posts.ts
+++ src/CRUD/create_posts.ts
@@ -2,9 +2,12 @@ import { prisma } from "../db"
 import { Request, Response } from "express"
 
 export const createPosts = async (req: Request, res: Response) => {
-  const { posterId, price, semester, bed, bathroom, ensuite, roommates, notes, locationId } = req.body
+  const { posterId, price, semester, bed, bathroom, ensuite, roommates, notes, locationId, line_1, line_2, town, city, county, eircode } = req.body
   const postResponse = await prisma.posts.create({
     data: { posterId, price, semester, bed, bathroom, ensuite, roommates, notes, locationId },
+  })
+  const addressResponse = await prisma.locations.create({
+    data: { post: { connect: { id: postResponse.id } }, line_1, line_2, town, city, county, eircode },
   })
   return postResponse
 }
