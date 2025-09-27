5070078debdf6fed1f50a8c5f77ef8a308e82e89
CiaranLy <lynchbros3@gmail.com>
2025-09-27 15:37:11 +0100

    corrected wrong origin

--- api/index.ts
+++ api/index.ts
@@ -16,7 +16,7 @@ const app = express()
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://isesass.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://isesass-frontend-2gmd2zugq-ciaranlys-projects.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
