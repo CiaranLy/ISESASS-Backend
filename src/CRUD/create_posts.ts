1a2503cbd6809df0c893b341e0b773163b5dc070
CiaranLy <lynchbros3@gmail.com>
2025-09-28 20:01:07 +0100

    logging for post creation

--- src/CRUD/create_posts.ts
+++ src/CRUD/create_posts.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express"
 const prisma = new PrismaClient()
 
 export const createPosts = async (req: Request, res: Response) => {
+  console.log(req.body)
   try {
     const { posterId, price, semester, bed, bathroom, ensuite, roommates, notes, line_1, line_2, line_3, city, county, eircode } = req.body
     const postResponse = await prisma.posts.create({
